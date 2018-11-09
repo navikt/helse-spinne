03a13db78f9c96363235acbddefb3bf591ef9eaa
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2018-11-09 09:14:06 +0100

    Log heap size during start

--- src/main/kotlin/no/nav/helse/App.kt
+++ src/main/kotlin/no/nav/helse/App.kt
@@ -1,5 +1,15 @@
 package no.nav.helse
 
+import org.slf4j.LoggerFactory
+
+private val log = LoggerFactory.getLogger("App")
+
 fun main() {
+    val heapSize = Runtime.getRuntime().totalMemory()
+    val heapMaxSize = Runtime.getRuntime().maxMemory()
+    val heapFreeSize = Runtime.getRuntime().freeMemory()
+
+    log.info("totalMemory = {}, maxMemory = {}, freeMemory = {)", heapSize, heapMaxSize, heapFreeSize)
+
     Behandling().start()
 }
