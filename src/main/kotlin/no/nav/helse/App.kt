f47a5756ad5db0d0428fc7158f0dcbbd30277c11
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2018-12-11 15:13:52 +0100

    turns out stream lib already has a shutdown hook

--- src/main/kotlin/no/nav/helse/App.kt
+++ src/main/kotlin/no/nav/helse/App.kt
@@ -1,19 +1,12 @@
 package no.nav.helse
 
 import org.slf4j.LoggerFactory
-import kotlin.concurrent.*
 
 private val log = LoggerFactory.getLogger("Spinne")
 
 fun main() {
     log.info("Starting Spinne")
 
-    val spinne = Spinne()
-
-    spinne.start()
-
-    Runtime.getRuntime().addShutdownHook(thread {
-        spinne.stop()
-    })
+    Spinne().start()
 }
 
